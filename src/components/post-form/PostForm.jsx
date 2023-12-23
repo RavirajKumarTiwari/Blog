42cfed37b8b2b13f05661a8db70305a3c0373da5
Raviraj Kumar <ravirajk545@gmail.com>
2023-12-23 21:19:55 +0530

    fix: Open any post after login

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -43,11 +43,7 @@ export default function PostForm({ post }) {
             if (file) {
                 const fileId = file.$id;
                 data.featuredimage = fileId;
-                const dbPost = await appwriteService.createPost({
-                    ...data,
-                    userId: userData.$id,
-                });
-
+                const dbPost = await appwriteService.createPost({...data, userId: userData.$id});
                 if (dbPost) {
                     navigate(`/post/${dbPost.$id}`);
                 }
